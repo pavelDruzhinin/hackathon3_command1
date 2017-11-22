2bf09ac8f588719697d310b07d0b7723bcf5e417
alxwt <alxwtf@gmail.com>
2017-11-22 20:57:11 +0300

    Отредактированы формы реги и входа

--- RosBets/Controllers/AccountController.cs
+++ RosBets/Controllers/AccountController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Security;
 using RosBets.Models;
 using RosBets.Context;
 
@@ -12,26 +13,64 @@ namespace RosBets.Controllers
     {
         RosBetsContext db = new RosBetsContext();
         // GET: Account
-        public ActionResult Registration()
+
+        //public ActionResult Login()
+        //{
+        //   return View();
+        //}
+        public ActionResult Registration(User User)
         {
-            return View();
+            User test = null;
+            if (ModelState.IsValid)
+            {
+                test = db.Users.FirstOrDefault(u => u.Mail == User.Mail);
+
+                if (test == null)
+                {
+                    db.Users.Add(User);
+                    db.SaveChanges();
+                    test = db.Users.Where(u => u.Mail == User.Mail && u.Password == User.Password).FirstOrDefault();
+                    if (test != null)
+                    {
+                        FormsAuthentication.SetAuthCookie(User.Mail, true);
+                        return RedirectToAction("Index", "Account");
+                    }
+                }
+            }
+            else
+            {
+                ModelState.AddModelError("", "Пользователь уже существует");
+            }
+            return View(User);
         }
 
-        public ActionResult Login()
+        public ActionResult Login(User user)
         {
-            return View();
-        }
-        public JsonResult Register(User User)
-        {
-            
-            db.Users.Add(User);
-            db.SaveChanges();
-            return new JsonResult {Data = User.Id};
+            if (ModelState.IsValid)
+            {
+                User test = null;
+                test = db.Users.FirstOrDefault(u => u.Mail == user.Mail && u.Password == user.Password);
+                if (test == null)
+                {
+                    ModelState.AddModelError("", "Пользователя с таким логином и паролем нет");
+                }
+                else
+                {
+                    FormsAuthentication.SetAuthCookie(user.Mail, true);
+                    return RedirectToAction("Index", "Account");
+                }
+            }
+            return View(user);
         }
 
-        public void Login(User user)
+        public string Index()
         {
-            
+            string result = "Вы не авторизованы";
+            if (User.Identity.IsAuthenticated)
+            {
+                result = "Ваш логин: " + User.Identity.Name;
+            }
+            return result;
         }
     }
 }
