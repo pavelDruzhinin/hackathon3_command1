d2f6d19fea9f575133207d0287cc3f06916a384a
Aleksandr Kougia <zero0072002@mail.ru>
2017-11-27 23:18:17 +0300

    fix

---
+++ RosBets/Scripts/SearchHistory.js
@@ -0,0 +1,35 @@
+﻿        $(document).ready(function () {
+            $('#show').click(function () {
+                var obj = {
+                    searchName: $(this).val('#bets'),
+                    searchSurname: $(this).val('#results')
+                };
+                $.ajax({
+                    type: 'POST',
+                    url: '/Account/CategorySearch',
+                    data: JSON.stringify(obj),
+                    contentType: 'application/json; charset=UTF-8',
+                    success: function (response) {
+                        console.log(response);
+                        var newHtml = response.map(function (el) {
+                            return createPr(el);
+                        }).join("");
+                        $('#ourtable tbody').html(newHtml);
+                    },
+                    error: function (response) {
+                        console.log(response);
+                    }
+                });
+            });
+
+        });
+
+
+
+            function createPr(bet) {
+                return '<tr><th>' +
+                    bet.Date + '</th><th>' + bet.Success + '</th><th>' + bet.TotalCoefficient + '</th><td>' +
+                    '</th><th>' + bet.Amount + '</th>';
+            }
+
+
