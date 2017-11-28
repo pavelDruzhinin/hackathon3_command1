eba11940db0cabff435dff644821ea1fee89ef4d
Aleksandr Kougia <zero0072002@mail.ru>
2017-11-28 20:46:27 +0300

    fix

---
+++ RosBets/ViewModel/MainViewModel.cs
@@ -0,0 +1,15 @@
+﻿using RosBets.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RosBets.ViewModel
+{
+    public class MainViewModel
+    {
+        public List<Sport> SportList { get; set; }
+        public News News { get; set; }
+    }
+    //комментарий
+}
