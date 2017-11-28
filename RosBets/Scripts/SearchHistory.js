be3090b42b9840f597e1ce082f269ae4137d9a55
Aleksandr Kougia <zero0072002@mail.ru>
2017-11-28 22:23:08 +0300

    fix

--- RosBets/Scripts/SearchHistory.js
+++ RosBets/Scripts/SearchHistory.js
@@ -1,35 +1,35 @@
-﻿        $(document).ready(function () {
-            $('#show').click(function () {
-                var obj = {
-                    type: $("#bets").val(),
-                    results: $("#results").val()
-                };
-                $.ajax({
-                    type: 'POST',
-                    url: '/Account/CategorySearch',
-                    data: JSON.stringify(obj),
-                    contentType: 'application/json; charset=UTF-8',
-                    success: function (response) {
-                        console.log(response);
-                        var newHtml = response.map(function (el) {
-                            return createPr(el);
-                        }).join("");
-                        $('#ourtable tbody').html(newHtml);
-                    },
-                    error: function (response) {
-                        console.log(response);
-                    }
-                });
-            });
-
-        });
-
-
-
-            function createPr(bet) {
-                return '<tr><th>' +
-                    bet.MatchName + '</th><th>' + bet.Success + '</th><th>' + bet.TotalCoefficient + '</th><td>' +
-                    '</th><th>' + bet.Success + '</th></tr>';
+﻿$(document).ready(function () {
+    $('#show').click(function () {
+        var obj = {
+            type: $("#bets").val(),
+            results: $("#results").val()
+        };
+        $.ajax({
+            type: 'POST',
+            url: '/Account/CategorySearch',
+            data: JSON.stringify(obj),
+            contentType: 'application/json; charset=UTF-8',
+            success: function (response) {
+                console.log(response);
+                var newHtml = response.map(function (el) {
+                    return createPr(el);
+                }).join("");
+                $('#ourtable tbody').html(newHtml);
+            },
+            error: function (response) {
+                console.log(response);
             }
+        });
+    });
+
+});
+
+
+
+function createPr(bet) {
+    return '<tr><th>' +
+        bet.MatchName + '</th><th>' + bet.Success + '</th><th>' + bet.TotalCoefficient + '</th><td>' +
+        '</th><th>' + bet.Success + '</th></tr>';
+}
 
 
