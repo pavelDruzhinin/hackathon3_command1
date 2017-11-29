926c80ef63b0740d356e61a8d41e36c185fdda22
BasilDaniel <vasilev.d.s@mail.ru>
2017-11-29 20:45:22 +0300

    line_js v.1.0

--- RosBets/App_Start/BundleConfig.cs
+++ RosBets/App_Start/BundleConfig.cs
@@ -21,6 +21,7 @@ namespace RosBets
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.js",
+                      "~/Scripts/_ros.bets.js",
                       "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
---
+++ RosBets/Scripts/_ros.bets.js
@@ -0,0 +1,8 @@
+﻿$(".table-td").click(function () {
+    $(this).toggleClass("clicked")
+    var tags = $("[class=clicked]")
+    console.log(tags[0])       
+    
+    //$.post("demo_test_post.asp",{name: "Donald Duck",city: "Duckburg"},function (data, status) {alert("Data: " + data + "\nStatus: " + status);});
+
+ });
