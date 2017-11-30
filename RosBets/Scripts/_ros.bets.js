792509a77f4a8c5fc72c3375bb23e6f3d0e66ce1
BasilDaniel <vasilev.d.s@mail.ru>
2017-11-30 20:40:15 +0300

    coupon tunning v.2.0

--- RosBets/Scripts/_ros.bets.js
+++ RosBets/Scripts/_ros.bets.js
@@ -1,23 +1,36 @@
 ﻿$(document).ready(function () {
+    var ids = [];
+    $(".table-td").click(toggleClick);
+              
+});
+function toggleClick() {
+    $(this).toggleClass("clicked");
+    postCoupon();
+}
 
-    $(".table-td").click(function () {
-        $(this).toggleClass("clicked");
-        var ids = [];
-        $(".clicked").each(function () {
-            ids.push(this.id);
-        });
-
-        $.ajax({
-            url: "/Bet/GetCoupon",
-            type: "POST",
-            dataType: "html",
-            contentType: "application/json",
-            data: JSON.stringify(ids),
-            success: function (data) {
-                $('.cupon-menu').html(data);
-            }
-        });
-
+function postCoupon() {   
+    ids = [];
+    $(".clicked").each(function () {
+        ids.push(this.id);
     });
 
-});
+    $.ajax({
+        url: "/Bet/GetCoupon",
+        type: "POST",
+        dataType: "html",
+        contentType: "application/json",
+        data: JSON.stringify(ids),
+        success: function (data) {
+            $('.cupon-menu').html(data);
+            console.log(JSON.stringify(ids));
+            $(".fa-coupon").click(toggleTd);
+        }
+    });
+
+} 
+
+function toggleTd() {
+    console.log(this.dataset.bindId);
+    $("#" + this.dataset.bindId).toggleClass("clicked");
+    postCoupon();
+}
