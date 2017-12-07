9e85e7d0f1bb4d24c7cb0220e4664122100c08f9
nucknine <nucknine@gmail.com>
2017-12-07 10:17:59 +0300

    commit

--- RosBets/Scripts/_ros.bets.js
+++ RosBets/Scripts/_ros.bets.js
@@ -1,57 +1,33 @@
-﻿// coupon action start
-$(document).ready(function () {
+﻿$(document).ready(function () {
     var ids = [];
-    $(".fa-coupon").each(function () {
-        ids.push(this.dataset.bindId);
-    });
-    $("#" + ids.join(",#")).addClass("clicked");
-
-    $(".table-td").click(addEvent);
-    $(document).on("click", ".fa-coupon", removeEvent);
+    $(".table-td").click(postCoupon);
+              
 });
 
-function addEvent() {
-    $(this).toggleClass("clicked");
-    $(this).siblings().removeClass("clicked");
-    var id = $(this).attr("id");
+function postCoupon() {  
+    $(this).toggleClass("clicked")
+    ids = [];
+    $(".clicked").each(function () {
+        ids.push(this.id);
+    });
+
     $.ajax({
-        url: "/Bet/AddToCoupon",
+        url: "/Bet/GetCoupon",
         type: "POST",
         dataType: "html",
         contentType: "application/json",
-        data: JSON.stringify({ id }),
+        data: JSON.stringify(ids),
         success: function (data) {
             $('.cupon-menu').html(data);
+            console.log(JSON.stringify(ids));
+            $(".fa-coupon").click(deleteCoupon);
         }
     });
 
+} 
+
+function deleteCoupon() {
+    console.log(this.dataset.bindId);
+    $("#" + this.dataset.bindId).toggleClass("clicked");
+    postCoupon();
 }
-
-function removeEvent() {
-    $("#" + $(this).attr("data-bind-id")).removeClass("clicked");
-    var id = $(this).attr("id");
-    console.log(id);
-    $.ajax({
-        url: "/Bet/RemoveFromCoupon",
-        type: "POST",
-        dataType: "html",
-        contentType: "application/json",
-        data: JSON.stringify({ id }),
-        success: function (data) {
-            $('.cupon-menu').html(data);
-        }
-    });
-}
-// coupon action end
-
-
-//fixed header start
-
-$(window).load(function () { headerFix() });
-$(window).resize(function () { headerFix() });
-
-function headerFix() {
-    var headerHeight = $("#header").outerHeight() + "px";
-    $("section").css({ "padding-top": headerHeight });
-};
-//fixed header end
