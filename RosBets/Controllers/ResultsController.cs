1011b13bcee90904948ae1c3f6132e666100ef11
Aleksandr Kougia <zero0072002@mail.ru>
2017-12-10 23:23:42 +0300

    результаты матчей

--- RosBets/Controllers/ResultsController.cs
+++ RosBets/Controllers/ResultsController.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using RosBets.Context;
+using RosBets.Models;
+using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -8,10 +11,79 @@ namespace RosBets.Controllers
 {
     public class ResultsController : Controller
     {
-        // GET: Results
+        RosBetsContext db = new RosBetsContext();
+
+        //// GET: Results
         public ActionResult ShowResults()
         {
-            return View();
+
+            List<Sport> sports = db.Sports.
+                Where(x => x.Name != null)
+                .ToList();
+
+            return View(sports);
+        }
+
+
+        public ActionResult MyShowResults()
+        {
+            var existingUser = db.Users.FirstOrDefault(u => u.Mail == User.Identity.Name);
+
+            List<Match> results = db.Matches
+                .Where(x => x.Finished == true)
+                .Include(x => x.Championship)
+                .Include(z => z.Championship.Sport)
+                .OrderBy(x => x.Date)
+                .ToList();
+
+            return PartialView("_ShowResults", results);
+        }
+
+
+        [HttpPost]
+        public ActionResult MyShowResults(string date, string sport)
+        {
+            var existingUser = db.Users.FirstOrDefault(u => u.Mail == User.Identity.Name);
+
+            DateTime myDate = DateTime.Parse(date);
+
+            List<Match> results = db.Matches
+                .Where(x => x.Finished == true)
+                .Include(x => x.Championship)
+                .Include(z => z.Championship.Sport)
+                .Include(y => y.Championship.Sport.Name)
+                .OrderBy(x => x.Date)
+                .ToList();
+
+            switch (sport)
+            {
+                case "Все":
+                    results = results
+                   .Where(y => y.Date == myDate)
+                   .ToList();
+                    break;
+
+                case "Футбол":
+                    results = results
+                   .Where(x => x.Championship.Sport.Name == "Футбол")
+                   .Where(y => y.Date == myDate)
+                   .ToList();
+                    break;
+                case "Хоккей":
+                    results = results
+                   .Where(x => x.Championship.Sport.Name == "Футбол")
+                   .Where(y => y.Date == myDate)
+                   .ToList();
+                    break;
+            }
+
+            return PartialView("_ShowResults", results);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
         }
     }
 }
