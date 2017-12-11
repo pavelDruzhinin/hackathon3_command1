5c97dd642823c2b5e30f724ed8a50bc50fdb839b
nucknine <nucknine@gmail.com>
2017-12-11 16:37:42 +0300

    Backend Страницы результатов. Result/ShowResults

--- RosBets/Controllers/ResultsController.cs
+++ RosBets/Controllers/ResultsController.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using RosBets.Context;
+using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -8,10 +10,18 @@ namespace RosBets.Controllers
 {
     public class ResultsController : Controller
     {
+        RosBetsContext db = new RosBetsContext();
         // GET: Results
         public ActionResult ShowResults()
         {
-            return View();
+            //var results = (from u in db.Matches
+            //               where u.Finished == true
+            //               select u).ToList();
+
+
+            var results = db.Matches.Include(x => x.Championship).Where(c => c.Finished == true).OrderBy(x => x.Championship.Name).ToList();
+            
+            return View(results);
         }
     }
 }
